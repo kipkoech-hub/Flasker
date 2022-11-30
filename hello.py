f5ba84e8813735eae22a1cd34c5ca662a5d287a9
Brian <kipkoechb96@gmail.com>
2022-11-30 16:40:39 +0300

    database added

--- hello.py
+++ hello.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 #FLASK INSTANCE
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "my super secret key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///members.db'
+# Initialize database
+db = SQLAlchemy(app)
+
+#Create database model
+class Members(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(200), nullable=False)
+	date_created = db.Column(db.DateTime, default=datetime.utcnow)
+#Create a function to return a string when we add something
+def __repr__(self):
+	return '<name %r>' % self.id
 
 #Create  aform class
 class NamerForm(FlaskForm):
@@ -31,6 +46,26 @@ def user(name):
 def page_not_found(e):
 	return render_template('404.html'), 404
 
+#members
+@app.route('/members', methods=['POST', 'GET'])
+def members():
+	title = "Group members"
+
+	if request.method == "POST":
+		member_name = request.form['name']
+		new_member = Members(name=member_name)
+
+		#Push to database
+		try:
+			db.session.add(new_member)
+			db.session.commit()
+			return redirect('/members')
+		except:
+			return "There was an error adding new Member."
+	else:
+		members = Members.query.order_by(Members.date_created)
+		return render_template("members.html", title=title, members=members)
+
 
 #Internal Server Error
 @app.errorhandler(500)
@@ -47,6 +82,9 @@ def name():
 	if form.validate_on_submit():
 		name = form.name.data
 		form.name.data = ''
+		flash("Form Submitted Successfully!!")
+
+
 	return render_template("name.html",
 		name = name,
 		form = form)
